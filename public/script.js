e06c9491973050c15311dbc10fcbe234af3354a9
gustavorods <gustavo.rlsilva07@gmail.com>
2024-07-12 18:58:00 -0300

    init

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const baseUrl = window.location.origin;
+
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const responseAPI = async (e) => {
+    e.preventDefault()
+    const res = await fetch(`${baseUrl}/message`, { // URL relativa ao domínio atual
+        method: 'GET'
+    })
+    const data = await res.json();
+    input.value = data.message;
+};
+
+button.addEventListener("click", responseAPI);
