412bae5f588c4dbee95b75db1cdf2c756095204d
gustavorods <gustavo.rlsilva07@gmail.com>
2024-07-14 13:14:00 -0300

    inicializando o html junto com o server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
+// Serve index.html on the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Get data
 app.get('/message', (req, res) => {
     res.json({message: 'Hello world!'})
